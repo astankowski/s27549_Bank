7b07543939e1451207092e4c627e10aa83ddeb1b
s27549-pj <s27549@pjwstk.edu.pl>
2024-01-28 11:51:50 +0100

    start

---
+++ src/main/java/com/mpr/s27549_bank/S27549BankApplication.java
@@ -0,0 +1,13 @@
+package com.mpr.s27549_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S27549BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S27549BankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mpr/s27549_bank/S27549BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpr.s27549_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S27549BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
