022c15a5f6ade44820eae389b1825ab818eb6aa6
s27549-pj <s27549@pjwstk.edu.pl>
2024-01-31 17:05:39 +0100

    Konstruktor od teraz nie wymaga podania id (id jest przypisywane automatycznie)

--- src/main/java/com/mpr/s27549_bank/model/Account.java
+++ src/main/java/com/mpr/s27549_bank/model/Account.java
@@ -2,14 +2,21 @@ package com.mpr.s27549_bank.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class Account {
     private Integer id;
+    @NonNull
     private String pesel;
+    @NonNull
     private Integer balance;
+    @NonNull
     private Currency currency;
+    @NonNull
     private String name;
+    @NonNull
     private String surname;
 }
