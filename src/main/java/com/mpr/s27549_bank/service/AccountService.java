a18add12d5da37e4d647859193b7ac1e451f813e
s27549-pj <s27549@pjwstk.edu.pl>
2024-01-31 16:01:12 +0100

    Poprawa walidacji i logiki metody create

--- src/main/java/com/mpr/s27549_bank/service/AccountService.java
+++ src/main/java/com/mpr/s27549_bank/service/AccountService.java
@@ -3,8 +3,10 @@ package com.mpr.s27549_bank.service;
 import com.mpr.s27549_bank.exception.ValidationException;
 import com.mpr.s27549_bank.model.Account;
 import com.mpr.s27549_bank.repository.AccountRepository;
+import io.micrometer.common.util.StringUtils;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 
@@ -14,13 +16,13 @@ public class AccountService {
     private AccountRepository accountRepository;
 
     public Account create(Account account) {
-        if (account.getId() == null) throw new ValidationException("id", "Cannot be blank");
-        if (account.getPesel().isBlank()) throw new ValidationException("pesel", "Cannot be blank");
+        if (StringUtils.isBlank(account.getPesel())) throw new ValidationException("pesel", "Cannot be blank"); //account.GetPesel()  == null || account.getPesel().isBlank()
+        if (account.getPesel().length() != 11) throw new ValidationException("pesel", "Must be 11 characters long");
         if (account.getBalance() == null) throw new ValidationException("balance", "Cannot be blank");
         if (account.getBalance() < 0) throw new ValidationException("balance", "Cannot be negative");
         if (account.getCurrency() == null) throw new ValidationException("currency", "Cannot be blank");
-        if (account.getName().isBlank()) throw new ValidationException("name", "Cannot be blank");
-        if (account.getSurname().isBlank()) throw new ValidationException("surname", "Cannot be blank");
+        if (StringUtils.isBlank(account.getName())) throw new ValidationException("name", "Cannot be blank");
+        if (StringUtils.isBlank(account.getSurname())) throw new ValidationException("surname", "Cannot be blank");
 
         return AccountRepository.create(account);
     }
@@ -30,9 +32,7 @@ public class AccountService {
     }
 
     public Account getById(Integer id){
-        if (id == null){
-            throw new ValidationException("id", "cannot be null");
-        }
+        if (id == null) throw new ValidationException("id", "cannot be null");
 
         return accountRepository.getById(id)
                 .orElseThrow( ()-> new ValidationException("id", "account does not exist"));
