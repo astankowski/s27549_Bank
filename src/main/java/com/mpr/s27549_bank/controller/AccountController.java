a5943f055419aa0fb64d59b10f980296d96ff630
s27549-pj <s27549@pjwstk.edu.pl>
2024-01-31 16:45:19 +0100

    Dodanie obsługi błędów i poprawa logiki API

--- src/main/java/com/mpr/s27549_bank/controller/AccountController.java
+++ src/main/java/com/mpr/s27549_bank/controller/AccountController.java
@@ -1,5 +1,6 @@
 package com.mpr.s27549_bank.controller;
 
+import com.mpr.s27549_bank.exception.ValidationException;
 import com.mpr.s27549_bank.model.Account;
 import com.mpr.s27549_bank.service.AccountService;
 import lombok.AllArgsConstructor;
@@ -16,32 +17,44 @@ public class AccountController {
     private AccountService accountService;
     @GetMapping("/all")
     public ResponseEntity<List<Account>> getAllAccounts() {
-        List<Account> allAccounts = accountService.getAllAccounts();
-
-        return ResponseEntity.ok(allAccounts);
+        try {
+            List<Account> allAccounts = accountService.getAllAccounts();
+            return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(allAccounts);
+        } catch (ValidationException e) {
+            return ResponseEntity.status(HttpStatusCode.valueOf(404)).build();
+        }
     }
 
     //http://localhost:8080/account/{id}
     @GetMapping
     public ResponseEntity<Account> getAccountByParam(@RequestParam(required = true) Integer id) {
-        Account account = accountService.getById(id);
-
-        return ResponseEntity.ok(account);
+        try {
+            Account account = accountService.getById(id);
+            return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(account);
+        } catch (ValidationException e) {
+            return ResponseEntity.status(HttpStatusCode.valueOf(404)).build();
+        }
     }
 
     //http://localhost:8080/account?id={id}
     @GetMapping("/{id}")
     public ResponseEntity<Account> getAccountByPathVariable(@PathVariable Integer id) {
-        Account account = accountService.getById(id);
-
-        return ResponseEntity.ok(account);
+        try {
+            Account account = accountService.getById(id);
+            return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(account);
+        } catch (ValidationException e) {
+            return ResponseEntity.status(HttpStatusCode.valueOf(404)).build();
+        }
     }
 
     @PostMapping("/create")
     public ResponseEntity<Account> createAccount(@RequestBody Account account) {
-        Account createdAccount = accountService.create(account);;
-
-        return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(createdAccount);
+        try {
+            Account createdAccount = accountService.create(account);
+            return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(createdAccount);
+        } catch (ValidationException e) {
+            return ResponseEntity.status(HttpStatusCode.valueOf(400)).build();
+        }
     }
 
 
